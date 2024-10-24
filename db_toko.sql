3332c9858a2a12ac8971b6d6b33e9073a3379ae8
RizqiA17 <asyriansyah@gmail.com>
2024-10-24 18:09:54 +0700

    new db exemple

--- db_toko.sql
+++ db_toko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 23, 2024 at 02:39 PM
+-- Generation Time: Oct 24, 2024 at 11:07 AM
 -- Server version: 8.3.0
 -- PHP Version: 8.3.6
 
